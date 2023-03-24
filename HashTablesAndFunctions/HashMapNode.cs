981eade441dbd4760fc937c7fdc0710a931b9be1
somu3341 <somashekarvsv1996@gmail.com>
2023-03-24 16:07:31 +0530

    [somu]Add:Hash Table and Hash Functions.

---
+++ HashTablesAndFunctions/HashMapNode.cs
@@ -0,0 +1,85 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Dynamic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace HashTablesAndFunctions
+{
+    public class HashMapNode<K, V>
+    {
+        private int size;
+        private LinkedList<KeyValue<K, V>>[] items;
+        public HashMapNode(int size)
+        {
+            this.size = size;
+            items=new LinkedList<KeyValue<K, V>>[size];
+        }
+        public int GetArrayPosition(K key)
+        {
+            int position = key.GetHashCode() % size;
+            return Math.Abs(position);
+        }
+        public void Add(K key, V value)
+        {
+            int position = GetArrayPosition(key);
+            LinkedList<KeyValue<K, V>> linkedlist = GetLinkedList(position);
+            KeyValue<K, V>item=new KeyValue<K, V>() { Key = key, Value = value };
+            linkedlist.AddLast(item);
+        }
+        public LinkedList<KeyValue<K, V>> GetLinkedList(int position)
+        {
+            LinkedList<KeyValue<K, V>> linkedlist= items[position];
+                if (linkedlist ==null)
+            {
+                linkedlist = new LinkedList<KeyValue<K, V>>();
+                items[position]=linkedlist;
+            }
+                return linkedlist;
+        }
+        public V Get(K key)
+        {
+            int position = GetArrayPosition(key);
+            LinkedList<KeyValue<K, V>> linkedlis = GetLinkedList(position);
+            foreach (KeyValue<K, V> item in linkedlis)
+            {
+                if (item.Key.Equals(key))
+                {
+                    return item.Value;
+                }
+            }
+            return default(V);
+        }
+        public void Remove(K key)
+        {
+            int position = GetArrayPosition(key);
+            LinkedList<KeyValue<K, V>> LinkedList = GetLinkedList(position);
+            bool itemFound = false;
+            KeyValue<K, V> founditem=default(KeyValue<K, V>);
+            foreach (KeyValue<K, V> item in LinkedList)
+            {
+                if (item.Key.Equals(key))
+                {
+                    itemFound = true;
+                    founditem = item;
+                }
+            }
+            if (itemFound)
+            {
+                LinkedList.Remove(founditem);
+            }
+        }
+        public void Display()
+        {
+            foreach (var linkedList in items)
+            {
+                if (linkedList != null)
+                    foreach (KeyValue<K, V> keyvalue in linkedList)
+                    {
+                        Console.WriteLine(keyvalue.Key + " " + keyvalue.Value);
+                    }
+            }
+        }
+    }
+}
---
+++ HashTablesAndFunctions/Program.cs
@@ -0,0 +1,16 @@
+﻿using System;
+namespace HashTablesAndFunctions
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            HashMapNode<string, string> hashMapNode = new HashMapNode<string, string>(4);
+            hashMapNode.Add("0", "a");
+            hashMapNode.Add("1", "b");
+            hashMapNode.Add("2", "c");
+            hashMapNode.Add("3", "d");
+            hashMapNode.Display();
+        }
+    }
+}
