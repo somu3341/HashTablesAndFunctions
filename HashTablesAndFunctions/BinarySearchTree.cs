e47679617e3d5c83082935351ac7a4820a3ab585
somu3341 <somashekarvsv1996@gmail.com>
2023-03-24 16:58:25 +0530

    [somu]Add:Binary Search Tree.

---
+++ HashTablesAndFunctions/BinarySearchTree.cs
@@ -0,0 +1,91 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace HashTablesAndFunctions
+{
+    public class BinarySearchTree<T> where T: IComparable<T> 
+    {
+        public T Nodedata { get; set; }
+        public BinarySearchTree<T> LeftTree { get; set; }
+        public BinarySearchTree<T> RightTree { get; set; }
+        public BinarySearchTree(T nodeData)
+        {
+            this.Nodedata = nodeData;
+            this.LeftTree = null;
+            this.RightTree = null;
+        }
+        int leftCount = 0;
+        int rightCount = 0;
+        bool result = false;
+        public void Insert(T item)
+        {
+            T currentNodeValue = this.Nodedata;
+            if (currentNodeValue.CompareTo(item) > 0)
+            {
+
+                if (this.LeftTree == null)
+                {
+                    this.LeftTree = new BinarySearchTree<T>(item);
+                }
+                else
+                {
+
+                    this.LeftTree.Insert(item);
+                }
+            }
+            else
+            {
+                if (this.RightTree == null)
+                    this.RightTree = new BinarySearchTree<T>(item);
+                else
+                    this.RightTree.Insert(item);
+            }
+        }
+        public void Display()
+        {
+            if (this.LeftTree != null)
+            {
+                this.leftCount++;
+                this.LeftTree.Display();
+            }
+            Console.WriteLine(this.Nodedata.ToString());
+            if (this.RightTree != null)
+            {
+                this.rightCount++;
+                this.RightTree.Display();
+            }
+        }
+        public void GetSize()
+        {
+            Console.WriteLine("Size" + " " + (1 + this.leftCount + this.rightCount));
+        }
+        public bool IfExits(T element, BinarySearchTree<T> node)
+        {
+            if (node == null)
+            {
+                return false;
+            }
+            if (node.Nodedata.Equals(element))
+            {
+                Console.WriteLine("Found the element in BST " + " " + node.Nodedata);
+                return true;
+            }
+            else
+            {
+                Console.WriteLine("Current element is {0} ", node.Nodedata);
+            }
+            if (element.CompareTo(node.Nodedata) < 0)
+            {
+                IfExits(element, node.LeftTree);
+            }
+            if (element.CompareTo(node.Nodedata) > 0)
+            {
+                IfExits(element, node.RightTree);
+            }
+            return result;
+        }
+    }
+} 
--- HashTablesAndFunctions/Program.cs
+++ HashTablesAndFunctions/Program.cs
@@ -5,12 +5,30 @@ namespace HashTablesAndFunctions
     {
         static void Main(string[] args)
         {
-            HashMapNode<string, string> hashMapNode = new HashMapNode<string, string>(4);
-            hashMapNode.Add("0", "a");
-            hashMapNode.Add("1", "b");
-            hashMapNode.Add("2", "c");
-            hashMapNode.Add("3", "d");
-            hashMapNode.Display();
+            //HashMapNode<string, string> hashMapNode = new HashMapNode<string, string>(4);
+            //hashMapNode.Add("0", "a");
+            // hashMapNode.Add("1", "b");
+            //  hashMapNode.Add("2", "c");
+            // hashMapNode.Add("3", "d");
+            // hashMapNode.Display();
+            BinarySearchTree<int> binarySearchTree = new BinarySearchTree<int>(56);
+            binarySearchTree.Insert(30);
+            binarySearchTree.Insert(70);
+            binarySearchTree.Insert(22);
+            binarySearchTree.Insert(40);
+            binarySearchTree.Insert(60);
+            binarySearchTree.Insert(95);
+            binarySearchTree.Insert(11);
+            binarySearchTree.Insert(65);
+            binarySearchTree.Insert(3);
+            binarySearchTree.Insert(63);
+            binarySearchTree.Insert(67);
+            binarySearchTree.Display();
+            binarySearchTree.GetSize();
+            bool result = binarySearchTree.IfExits(67, binarySearchTree);
+            Console.WriteLine(result);
+
+
         }
     }
 }
